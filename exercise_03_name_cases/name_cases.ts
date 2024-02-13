da4593de5bf283b7f12983815d2ce5ee4d881628
Fahad warsi <fahad7swarsi@gmail.com>
2024-02-14 02:50:12 +0500

    add config file

---
+++ exercise_03_name_cases/name_cases.ts
@@ -0,0 +1,8 @@
+let personName: string = "fahad";
+
+console.log(personName.toLowerCase());
+console.log(personName.toUpperCase());
+
+console.log(
+  personName.replace(personName.charAt(0), personName.charAt(0).toUpperCase())
+);
