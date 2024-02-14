7ba7f752b4ec78433ed419fa753b5b3e687fd339
Fahad warsi <fahad7swarsi@gmail.com>
2024-02-14 21:13:54 +0500

    Add exercise_11 sol

---
+++ exercise_11_names/app.js
@@ -0,0 +1,7 @@
+// 11. Names: Store the names of a few of your friends in a array called names.
+// Print each person’s name by accessing each element in the list, one at a time.
+var names = ["Hameed", "Fahad", "Amin", "Ahmed"];
+for (var _i = 0, names_1 = names; _i < names_1.length; _i++) {
+    var i = names_1[_i];
+    console.log("Names = ", i);
+}
---
+++ exercise_11_names/app.ts
@@ -0,0 +1,10 @@
+// 11. Names: Store the names of a few of your friends in a array called names.
+// Print each person’s name by accessing each element in the list, one at a time.
+
+//Assign values
+let names: string[] = ["Hameed", "Fahad", "Amin", "Ahmed"];
+
+//print each element in the list.
+for (let i of names) {
+  console.log("Names = ", i);
+}
