ab2c08792816511a20b382bb311617c970a9ea2a
Fahad warsi <fahad7swarsi@gmail.com>
2024-02-14 23:00:51 +0500

    Add exercise 15 sol

---
+++ exercise_15_changing_guest_list/app.ts
@@ -0,0 +1,24 @@
+// 15. Changing Guest List: You just heard that one of your guests can’t make the dinner,
+//  so you need to send out a new set of invitations. You’ll have to think of someone else to invite.
+
+// • Start with your program from Exercise 14. Add a print statement at the end of your program stating the name of the guest who can’t make it.
+
+// • Modify your list, replacing the name of the guest who can’t make it with the name of the new person you are inviting.
+
+// • Print a second set of invitation messages, one for each person who is still in your list.
+
+let person: string[] = ["Hameed", "Fahad", "Amin"]; //Assign the values
+
+let message: string = " I'd like to invite you to dinner."; //Assign the message
+
+for (let i of person) {
+  console.log(`Mr. ${i}, ${message}`); //Print the a message to each person.
+}
+
+console.log(`Mr. ${person[1]} can't make the dinner.`); //Changing Guest List: You just heard that one of your guests can’t make the dinner,
+
+person.splice(1, 1, "Ahmed"); //replacing the guest name
+
+for (let i of person) {
+  console.log(`Mr. ${i}, ${message}`); //print a second set of invitation.
+}
