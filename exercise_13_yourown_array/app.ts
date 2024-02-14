daf34cfe3a2b5c32ac58371726f7b033b1cee75f
Fahad warsi <fahad7swarsi@gmail.com>
2024-02-14 22:21:07 +0500

    Add exercise_13 sol

---
+++ exercise_13_yourown_array/app.ts
@@ -0,0 +1,14 @@
+// 13. Your Own Array: Think of your favorite mode of transportation, such as a motorcycle or a car, and make a list that
+// stores several examples. Use your list to print a series of statements about these items, such as “I would like to own a Honda motorcycle.”
+
+//Assign values
+let list: string[] = [
+  "I would like to own a Honda motorcycle.",
+  "I would like to own a Civic Car.",
+  "I wish to plan Umrah with my family by Aeroplane.",
+];
+
+//printthe series of statements.
+for (let i of list) {
+  console.log(i);
+}
