2696a60e48aaac48b350a5a573d8df1f17b52d81
Fahad-warsi <149926558+Fahad-warsi@users.noreply.github.com>
2024-02-14 20:45:47 +0500

    Delete exercise_09_favorite_number directory

--- exercise_09_favorite_number/app.js
+++
@@ -1,5 +0,0 @@
-// 9. Favorite Number: Store your favorite number in a variable.
-// Then, using that variable, create a message that reveals your favorite number. Print that message.
-var favoriteNumber = 10; //store favorite number
-var message = "Your favorite number is "; //store message
-console.log("".concat(message, " ").concat(favoriteNumber)); // print message & favourite number.
