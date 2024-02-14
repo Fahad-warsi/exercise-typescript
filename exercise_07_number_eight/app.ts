9c6f6d5ebf7e62d608ca3b0436279077f8f510f6
Fahad warsi <fahad7swarsi@gmail.com>
2024-02-14 20:13:45 +0500

    Add exercise_07 sol

---
+++ exercise_07_number_eight/app.ts
@@ -0,0 +1,7 @@
+// 7. Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8.
+// Be sure to enclose your operations in print statements to see the results.
+
+console.log("addition = ", 5 + 3);
+console.log("subtraction = ", 11 - 3);
+console.log("multiplication = ", 4 * 2);
+console.log("division = ", 16 / 2);
