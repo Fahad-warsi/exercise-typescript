416d3dea802aec35f0fe584664005639ab253a79
Fahad warsi <fahad7swarsi@gmail.com>
2024-02-14 20:39:40 +0500

    Add exercise_09 sol

---
+++ exercise_09_favorite_number/app.ts
@@ -0,0 +1,8 @@
+// 9. Favorite Number: Store your favorite number in a variable.
+// Then, using that variable, create a message that reveals your favorite number. Print that message.
+
+let favoriteNumber: number = 10; //store favorite number
+
+let message: string = "Your favorite number is "; //store message
+
+console.log(`${message} ${favoriteNumber}`); // print message & favourite number.
