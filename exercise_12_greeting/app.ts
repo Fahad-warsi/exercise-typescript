07d5efacab8df82d9b120b2f8db3b2943af988f4
Fahad warsi <fahad7swarsi@gmail.com>
2024-02-14 22:01:05 +0500

    Add exercise_12 sol

---
+++ exercise_12_greeting/app.ts
@@ -0,0 +1,9 @@
+// 12. Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name,
+// print a message to them. The text of each message should be the same, but each message should be personalized with the person’s name.
+
+//assign value
+let names: string[] = ["Hameed", "Fahad", "Amin", "Ahmed"];
+//print each value with greeting.
+for (let i of names) {
+  console.log("Hi Mr.", i);
+}
