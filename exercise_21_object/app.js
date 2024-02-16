2802bde9d69ca1d788f281e7e72e1d166f1ca9d5
Fahad warsi <fahad7swarsi@gmail.com>
2024-02-16 07:34:19 +0500

    Add exercise_21 sol

---
+++ exercise_21_object/app.js
@@ -0,0 +1,11 @@
+// 21. They think of something you could store in a TypeScript Object.
+// Write a program that creates Objects containing these items.
+var category = {
+    countries: ["Pakistan", "Inida", "Saudia Arabia"],
+    cities: ["Karachi", "Lahore", "Faislabad"],
+    mountain: ["K2", "Everest", "Nanga Parbat"],
+    river: ["Hub River", "Jhelum River", "Indus River"],
+    language: ["Punjabi", "Urdu", "Sindhi"],
+    fav_transport: ["Bullet Train", "Helicopter", "Boat"],
+};
+console.log(category);
