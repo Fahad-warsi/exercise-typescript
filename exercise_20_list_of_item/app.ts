a4d7a471114a4b31724e65c15a85d11505141006
Fahad warsi <fahad7swarsi@gmail.com>
2024-02-16 07:24:07 +0500

    Add exercise 20 sol

---
+++ exercise_20_list_of_item/app.ts
@@ -0,0 +1,23 @@
+// 20. Think of something you could store in a array. For example, you could make a list of mountains, rivers, countries, cities, languages,
+// or anything else you’d like. Write a program that creates a list containing these items.
+
+//Categories
+let category: string[][] = [
+  ["Pakistan", "Inida", "Saudia Arabia"],
+  ["Karachi", "Lahore", "Faislabad"],
+  ["K2", "Everest", "Nanga Parbat"],
+  ["Hub River", "Jhelum River", "Indus River"],
+  ["Punjabi", "Urdu", "Sindhi"],
+  ["Bullet Train", "Helicopter", "Boat"],
+];
+
+// print the Countries and cities
+console.log("List of Countries and Cities:");
+category.forEach((category, categoryindex) => {
+  console.log(`Category ${categoryindex + 1}:`);
+
+  category.forEach((item, itemindex) => {
+    console.log(`  ${itemindex + 1}. ${item}`);
+  });
+  console.log(``); // Add a newline between categories
+});
