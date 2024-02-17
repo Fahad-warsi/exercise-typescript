ffaab8b0a4e03c57d41b9a4a3b0e27eb0be218e0
Fahad warsi <fahad7swarsi@gmail.com>
2024-02-17 10:25:17 +0500

    Add exercise_44 sol

---
+++ exercise_44_sandwich/app.js
@@ -0,0 +1,19 @@
+// 44.Sandwiches: Write a function that accepts a array of items a person wants on a sandwich.
+// The function should have one parameter that collects as many items as the function call provides, and
+// it should print a summary of the sandwich that is being ordered.
+//  Call the function three times, using a different number of arguments each time.
+function make_sandwich() {
+    var items = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        items[_i] = arguments[_i];
+    }
+    console.log("Sandwich is being ordered:");
+    for (var i = 0; i < items.length; i++) {
+        console.log("".concat(i + 1, ". ").concat(items[i]));
+    }
+    console.log("Sandwich is ready.......!!!");
+    console.log("");
+}
+make_sandwich("Chicken", "Olive", "cheese", "Mayo");
+make_sandwich("Beef", "Egg", "tomato", "cocumber");
+make_sandwich("Bread", "Egg");
